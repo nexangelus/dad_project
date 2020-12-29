2b4363aebd98bce4aa1b49d388323a18fa7cfe77
André Paulo <andre_duivenvoorden@hotmail.com>
2020-12-29 16:42:32 +0000

    todo switch all socket calls to listeners on users/orders

---
+++ laravel-vue/app/Listeners/OrderSaveListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Order;
+
+class OrderSaveListener {
+
+    /* @var Order $order */
+    public function __construct(Order $order) {
+        // TODO
+    }
+
+}
--- laravel-vue/resources/js/components/navbar.vue
+++ laravel-vue/resources/js/components/navbar.vue
@@ -54,7 +54,7 @@ export default {
             axios.post('/api/logout', this.credentials).then(response => {
                 this.$store.commit("clearUser");
                 Vue.toasted.success(`${response.data.msg}`)
-                this.$router.push({name: 'main'})
+                this.$router.push({name: 'main'}) // TODO não é preciso fazer push para a main se já tiver na main
             }).catch(error => {
                 Vue.toasted.error(error.response.data.message)
             })
