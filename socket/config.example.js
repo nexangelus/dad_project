cd077943510f4dc55386471719bc508521afd524
André Paulo <andre_duivenvoorden@hotmail.com>
2020-12-30 19:47:51 +0000

    🚩🚩 Added axios to SocketIO. REQUIRED CONFIG FOR .env and Config Socket

---
+++ socket/config.example.js
@@ -0,0 +1,4 @@
+module.exports =  {
+    "API_URL": "http://projeto-dad.test/",
+    "API_PW": "VUy4!DZn40k9f2PMZTp%Ovf7JUru39rr4oFI@Z!GibN@S!tpUjOf2F^q&IqIyx3!bThM12mjO0IyoCiqc2D9*l1v$!6R7^#Zaa7k6BYFneEdO8LH2s1J8I2M@vQ26inV"
+}
